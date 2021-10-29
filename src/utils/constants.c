3a80e71aeb1d71ab792f8baaa8d35644f3d93823
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-10-29 17:21:33 -0300

    feat(utils): Cria arquivo de constantes

---
+++ src/utils/constants.c
@@ -0,0 +1,12 @@
+#include "constants.h"
+
+const int EXPRESSION_SIZE = 101;
+
+const int VALID_YES_SIZE = 2;
+const char VALID_YES[2] = {'s', 'S'};
+
+const int VALID_NO_SIZE = 2;
+const char VALID_NO[2] = {'n', 'N'};
+
+const int VALID_OPERATIONS_SIZE = 7;
+const char VALID_OPERATIONS[7] = {'+', '-', '*', 'x', 'X', '^', '\\'};
---
+++ src/utils/constants.h
@@ -0,0 +1,10 @@
+extern const int EXPRESSION_SIZE;
+
+extern const int VALID_YES_SIZE;
+extern const char VALID_YES[2];
+
+extern const int VALID_NO_SIZE;
+extern const char VALID_NO[2];
+
+extern const int VALID_OPERATIONS_SIZE;
+extern const char VALID_OPERATIONS[7];
