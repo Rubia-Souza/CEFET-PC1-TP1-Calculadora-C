c32d58a95cc54e177b64ab74117d98cdb6ad65aa
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-10-30 10:56:45 -0300

    feat(utils): Change max amount of numbers and add / to division

--- src/utils/constants.c
+++ src/utils/constants.c
@@ -4,4 +4,4 @@ const char VALID_YES[VALID_YES_SIZE] = {'s', 'S'};
 
 const char VALID_NO[VALID_NO_SIZE] = {'n', 'N'};
 
-const char VALID_OPERATIONS[VALID_OPERATIONS_SIZE] = {'+', '-', '*', 'x', 'X', '^', '\\'};
+const char VALID_OPERATIONS[VALID_OPERATIONS_SIZE] = {'+', '-', '*', 'x', 'X', '^', '/', '\\'};
--- src/utils/constants.h
+++ src/utils/constants.h
@@ -1,10 +1,12 @@
-#define EXPRESSION_SIZE 101
+#define EXPRESSION_SIZE 61
+#define MAX_OPERATION_NUMBERS 20
+
 #define VALID_YES_SIZE 2
 #define VALID_NO_SIZE 2
-#define VALID_OPERATIONS_SIZE 7
+#define VALID_OPERATIONS_SIZE 8
 
-extern const char VALID_YES[2];
+extern const char VALID_YES[VALID_YES_SIZE];
 
-extern const char VALID_NO[2];
+extern const char VALID_NO[VALID_NO_SIZE];
 
-extern const char VALID_OPERATIONS[7];
+extern const char VALID_OPERATIONS[VALID_OPERATIONS_SIZE];
