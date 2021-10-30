9d8e4326d87b790534635be67e4b2406c1e07d96
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-10-30 08:44:26 -0300

    refactor(utils): Arruma declaração das constantes

--- src/utils/constants.c
+++ src/utils/constants.c
@@ -1,12 +1,7 @@
 #include "constants.h"
 
-const int EXPRESSION_SIZE = 101;
+const char VALID_YES[VALID_YES_SIZE] = {'s', 'S'};
 
-const int VALID_YES_SIZE = 2;
-const char VALID_YES[2] = {'s', 'S'};
+const char VALID_NO[VALID_NO_SIZE] = {'n', 'N'};
 
-const int VALID_NO_SIZE = 2;
-const char VALID_NO[2] = {'n', 'N'};
-
-const int VALID_OPERATIONS_SIZE = 7;
-const char VALID_OPERATIONS[7] = {'+', '-', '*', 'x', 'X', '^', '\\'};
+const char VALID_OPERATIONS[VALID_OPERATIONS_SIZE] = {'+', '-', '*', 'x', 'X', '^', '\\'};
--- src/utils/constants.h
+++ src/utils/constants.h
@@ -1,10 +1,10 @@
-extern const int EXPRESSION_SIZE;
+#define EXPRESSION_SIZE 101
+#define VALID_YES_SIZE 2
+#define VALID_NO_SIZE 2
+#define VALID_OPERATIONS_SIZE 7
 
-extern const int VALID_YES_SIZE;
 extern const char VALID_YES[2];
 
-extern const int VALID_NO_SIZE;
 extern const char VALID_NO[2];
 
-extern const int VALID_OPERATIONS_SIZE;
 extern const char VALID_OPERATIONS[7];
